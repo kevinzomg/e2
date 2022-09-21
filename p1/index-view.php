0dedfb91ee9d6120d510302da3b1ee34d36e4895
Kevin Zong <kzong@g.harvard.edu>
2022-09-21 06:46:40 +0000

    Fleshed out HTML, added basic PHP and CSS

---
+++ p1/index-view.php
@@ -0,0 +1,115 @@
+<?php include_once('index.php');?>
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <title>Project 1</title>
+    <meta charset='utf-8'>
+    <link href=data:, rel=icon>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <h1>Project 1</h1>
+
+    <h2>Mechanics</h2>
+    <ul>
+        <li>Test your luck and try to get the highest score within 3 tries</li>
+        <li>The game starts as a 3x3 grid, each section represented by a number 1-9; there are no repeated numbers</li>
+        <li>One number is revealed initially, and the player will have 3 chances to uncover a number</li>
+        <ul>
+            <li>For the purposes of this assignment, all numbers will be revealed</li>
+        </ul>
+        <li>After uncovering 3 numbers for a total of 4 revealed numbers, the player will then choose one line of
+            numbers</li>
+        <li>Each number in a line is added together, and the final tally will determine the amount of points the player
+            will receive</li>
+        <ul>
+            <li>For the purposes of this assignment, the row with the lowest total value will be picked, unless one of
+                the "jackpot" number combinations is present</li>
+        </ul>
+    </ul>
+
+    <h2>Results</h2>
+    <table>
+        <tr>
+            <td>
+                <h3><?php print $numbers[0];?></h3>
+            </td>
+            <td>
+                <h3><?php print $numbers[1];?></h3>
+            </td>
+            <td>
+                <h3><?php print $numbers[2];?></h3>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <h3><?php print $numbers[3];?></h3>
+            </td>
+            <td>
+                <h3><?php print $numbers[4];?></h3>
+            </td>
+            <td>
+                <h3><?php print $numbers[5];?></h3>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <h3><?php print $numbers[6];?></h3>
+            </td>
+            <td>
+                <h3><?php print $numbers[7];?></h3>
+            </td>
+            <td>
+                <h3><?php print $numbers[8];?></h3>
+            </td>
+        </tr>
+    </table>
+
+    <h2>Payouts</h2>
+    <ul id="sumlist">Sum
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>10</li>
+        <li>11</li>
+        <li>12</li>
+        <li>13</li>
+        <li>14</li>
+        <li>15</li>
+        <li>16</li>
+        <li>17</li>
+        <li>18</li>
+        <li>19</li>
+        <li>20</li>
+        <li>21</li>
+        <li>22</li>
+        <li>23</li>
+        <li>24</li>
+    </ul>
+    <ul id="payoutlist">Payouts
+        <li>10000</li>
+        <li>36</li>
+        <li>720</li>
+        <li>360</li>
+        <li>80</li>
+        <li>252</li>
+        <li>108</li>
+        <li>72</li>
+        <li>54</li>
+        <li>180</li>
+        <li>72</li>
+        <li>180</li>
+        <li>119</li>
+        <li>36</li>
+        <li>306</li>
+        <li>1080</li>
+        <li>144</li>
+        <li>1800</li>
+        <li>3600</li>
+    </ul>
+</body>
+
+</html>
--- p1/index.php
+++ p1/index.php
@@ -1,24 +1,37 @@
-<!DOCTYPE html>
-<html lang='en'>
+<?php 
 
-<head>
-    <title>Project 1</title>
-    <meta charset='utf-8'>
-    <link href=data:, rel=icon>
-</head>
 
-<body>
-    <h1>Project 1</h1>
+$numbers = array(1, 2, 3, 4, 5, 6, 7, 8, 9);
+shuffle($numbers);
+var_dump($numbers);
 
-    <h2>Mechanics</h2>
-    <ul>
-        <li></li>
-    </ul>
+echo current($numbers) . "<br>";
+echo next($numbers);
 
-    <h2>Results</h2>
-    <ul>
-        <li></li>
-    </ul>
-</body>
+$row_1 = ($numbers[0] + $numbers[1] + $numbers[2]);
+//print "<br>row 1 total is $row_1 <br>";
 
-</html>
+$row_2 = ($numbers[3] + $numbers[4] + $numbers[5]);
+//print "row 2 total is $row_2<br>";
+
+$row_3 = ($numbers[6] + $numbers[7] + $numbers[8]);
+//print "row 3 total is $row_3<br>";
+
+$column_1 = ($numbers[0] + $numbers[3] + $numbers[6]);
+//print "column 1 total is $column_1<br>";
+
+$column_2 = ($numbers[1] + $numbers[4] + $numbers[7]);
+//print "column 2 total is $column_2<br>";
+
+$column_3 = ($numbers[2] + $numbers[5] + $numbers[8]);
+//print "column 3 total is $column_3<br>";
+
+$diagonal_1 = ($numbers[0] + $numbers[4] + $numbers[8]);
+//print "diagonal 1 total is $diagonal_1<br>";
+
+$diagonal_2 = ($numbers[2] + $numbers[4] + $numbers[6]);
+//print "diagonal 2 total is $diagonal_2<br>";
+
+
+
+require "index-view.php";
